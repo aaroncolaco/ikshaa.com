48d827307025a7b3f164ac2f8a78a67dd052687f
aarongoa <taipan93@gmail.com>
2014-11-05 17:18:40 +0530

    Email is displayed on different lines. Now the form verification isn't working. Commented out the jquery verification codes that I tried. They don't work. They send the data as GET even though POST is specified.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,36 @@
 var main = function() {
 	
 	$('#submitButton').click(function() { 
+
+		// $('#form')[0].checkValidity();
+		// $('#form')[1].checkValidity();
+		// if(!$('#form')[0].checkValidity()) {
+		//   code here
+		// }
+		// if(!$('#form')[1].checkValidity()) {
+		//   code here
+		// }
+
+		// var form = $('#form');
+		// form.validate();
+		// if(form.valid()) {
+		//	//code here
+		// }
+
+		// if ($('#name').val.equal('')) {			//even this causes the same problem!!
+		// 	return;
+		// };
+		// if ($('#email').val.equal('')) {
+		// 	return;
+		// };
+
+		// if( document.form.name.value == "" )
+		// {
+		// 	alert( "Please provide your name!" );
+		// 	document.form.name.focus() ;
+		// 	return;
+		// }
+
 		var name = $('#name').val();
 		var email = $('#email').val();
 		var number1 = $('#number1').val();
@@ -9,13 +39,13 @@ var main = function() {
 		var designationText= $('#designationText').val();
 		var message = $('#message').val();
 
-		var messageToSend = 'Name= ' + name + '\nEmail= ' + email + '\nLandline= ' + 
-			number1 + '\nCell Phone= ' + number2 + '\nCompany= ' + company +
-			'\nDesignation= ' + designationText + '\nMessage= ' + message;
+		var messageToSend = "Name: " + name + "\nEmail: " + email + "\nLandline: " +	//have to use double quotes since PHP detects  
+			number1 + "\nCell Phone: " + number2 + "\nCompany: " + company +			//escape sequences like \n only in double quotes
+			"\nDesignation: " + designationText + "\nMessage: " + message;
 
-		var dataString = 'Name= ' + name + '&Email= ' + email + '&MessageToSend= ' + messageToSend;
+		var dataString = "Name=" + name + "&Email=" + email + "&MessageToSend=" + messageToSend;
 
-		alert (dataString);//return false;
+		//alert (dataString);return false; 	//uncomment to display the data that will be sent to the php script on button click
 
 		$.ajax({
 		    type: "post",
@@ -28,6 +58,7 @@ var main = function() {
 		});
 		
 		return false;
+
 	});
 }
 
--- php/mail.php
+++ php/mail.php
@@ -1,22 +1,19 @@
 <?php
        // from the form
-       $name = $_POST['Name'];
-       $email = $_POST['Email'];
-       $message = $_POST['MessageToSend'];
+       $name = $_POST["Name"];
+       $email = $_POST["Email"];
+       $message = $_POST["MessageToSend"];
 
        // set here
        $subject = "Reservation";
-       $to = 'taipan93@gmail.com';
-
-//        $body = <<<HTML
-// $message
-// HTML;
+       $to = "taipan93@gmail.com";
 
        $headers = "From: $email\r\n";
-       $headers .= "Content-type: text/html\r\n";
+       $headers .= "Content-type: text/plain\r\n";		/* needs to be text/plain and NOT text/html so that the newline characters work. 
+       														Otherwise received email is in one line */
 
-       // send the email
-       mail($to, $subject, $message, $headers);
+		// send the email
+		mail($to, $subject, $message, $headers);
        
 		#redirect
 		header( "Location: http://aarongoa.net84.net/#contact" );
