8c20eecf593dfc3328419e5a31f59914a6d11050
aarongoa <taipan93@gmail.com>
2014-11-19 14:52:18 +0530

    Form verification fixed. Instead of setting button click listener, used a submit listener for the entire form. Now it validates like the normal HTML5 method.

--- js/script.js
+++ js/script.js
@@ -1,61 +1,25 @@
 $(document).ready(function() {
-	
-	$('#submitButton').click(function() { 
 
-		// $('#form')[0].checkValidity();
-		// $('#form')[1].checkValidity();
-		// if(!$('#form')[0].checkValidity()) {
-		//   code here
-		// }
-		// if(!$('#form')[1].checkValidity()) {
-		//   code here
-		// }
+	$('#form').on('submit', function(ev) {
 
-		// var form = $('#form');
-		// form.validate();
-		// if(form.valid()) {
-		//	//code here
-		// }
+	    // prevent from auto-submitting  
+	    ev.preventDefault();
+	    ev.stopPropagation();
 
-		var $flag = true;
-		//hide error message if it is showing
-		$('#nameCheck').hide();
-		$('#emailCheck').hide();
-
-		if ($('#name').val() === '') {          //even this causes the same problem!!
-			$flag = false;
-			$('#nameCheck').show();		//show the error message
-		};
-		if ($('#email').val() === '') {
-			$flag = false;
-			$('#emailCheck').show();
-		};
-
-		// if( document.form.name.value == "" )
-		// {
-		// 	alert( "Please provide your name!" );
-		// 	document.form.name.focus() ;
-		// 	return;
-		// }
-
-		if ($flag) {
-			$('#nameCheck').hide();
-			$('#emailCheck').hide();
-			sendData();
-		};
-
-		return false;
+	    // if all is good, then submit.
+	    sendData();
 
 	});
 
 	$(".nav a").on("click", function(){
 		// so that the clicked one gets highlighted
-	   $(".nav").find(".active").removeClass("active");
-	   $(this).parent().addClass("active");
-	   //to hide the menu after clicking
-	   $('.navbar-collapse').collapse('hide');
+		$(".nav").find(".active").removeClass("active");
+		$(this).parent().addClass("active");
+		//to hide the menu after clicking
+		$('.navbar-collapse').collapse('hide');
 	});
 	
+	//function to send data to the PHP script
 	var sendData = function() { 
 		var name = $('#name').val();
 		var email = $('#email').val();
@@ -74,13 +38,13 @@ $(document).ready(function() {
 		//alert (dataString);return false; 	//uncomment to display the data that will be sent to the php script on button click
 
 		$.ajax({
-		    type: "post",
-		    url: "php/mail.php",
-		    data: dataString,
-		    success: function() {
-		      $('.form').hide()
-		      $('#changingText').text("Thank you. We will be in touch with you shortly " + name)
-		    }
+			type: "post",
+			url: "php/mail.php",
+			data: dataString,
+			success: function() {
+				$('.form').hide()
+				$('#changingText').text("Thank you. We will be in touch with you shortly " + name)
+			}
 		});
 		
 		return;
